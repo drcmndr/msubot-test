61be0393c542327505ac83354eb2ce67b0e3c8d5
Johaymen <johaymen.malawani@g.msuiit.edu.ph>
2025-02-19 20:37:54 +0800

    test live

--- app.py
+++ app.py
@@ -137,6 +137,7 @@ CORS(app,
                  "http://localhost:5500",
                  "http://127.0.0.1:5500",
                  "https://msubot-test.onrender.com",  # Your Render backend URL
+                 "https://drcmndr.github.io", # Your Pages frontend URL
                  "*"  # Temporarily allow all origins for testing
              ],
              "methods": ["GET", "POST", "OPTIONS"],
